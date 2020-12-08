8e7f1614b0f9d496e0a3e7d204427bdba3ddb8d0
suejkim <abbccc0225@daum.net>
2020-12-08 23:44:08 +0900

    Initial Commit

---
+++ Bounty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bounty
+//
+//  Created by sjkim on 2020/12/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
