8d840e834b7969b63b306ab3ae45e322e05c499e
suejkim <abbccc0225@daum.net>
2020-12-14 23:46:05 +0900

    UITableViewDataSource, UITableViewDelegate 추가

--- Bounty/BountyViewController.swift
+++ Bounty/BountyViewController.swift
@@ -7,23 +7,48 @@
 
 import UIKit
 
-class BountyViewController: UIViewController {
+class BountyViewController: UIViewController,
+                            UITableViewDataSource,
+                            UITableViewDelegate{
 
+    let nameList = ["brook", "chopper", "franky", "luffy", "nami", "robin", "sanji", "zoro"]
+    let bountyList = [33000000, 50, 44000000, 300000000, 16000000, 80000000, 77000000, 120000000]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // UITableViewDataSource
+    
+    // 리턴할 데이터 개수
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bountyList.count
     }
-    */
+    
+    // indexPath: cell의 위치
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
+            return UITableViewCell()
+        }
+        let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
+        cell.imgView.image = img
+        cell.nameLabel.text = nameList[indexPath.row]
+        cell.bountyLabel.text = "\(bountyList[indexPath.row])"
+        
+        return cell
+    }
+    
+    
+    // UITableViewDelegate
+    // 클릭시 일어날 일
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("--> \(indexPath.row)")
+    }
+}
 
+class ListCell: UITableViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bountyLabel: UILabel!
 }
