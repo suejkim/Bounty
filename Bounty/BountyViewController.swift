ff929fbf315a374be4532e49c9489cd55ed4e2ad
suejkim <abbccc0225@daum.net>
2020-12-14 22:18:33 +0900

    table view constraint 적용 및 table view cell 추가

---
+++ Bounty/BountyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BountyViewController.swift
+//  Bounty
+//
+//  Created by sjkim on 2020/12/14.
+//
+
+import UIKit
+
+class BountyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
